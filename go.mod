f5fa9655272ac7238164dca0fd4990b81ff4a0fd
lnix1 <louis.nix2@gmail.com>
2025-12-13 07:43:24 -0800

    adding chapter 1 materials

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lnix1/Chirpy
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+	"log"
+)
+
+type ResponseData struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
+func main() {
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz/", handlerReadiness)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
